5b2779cd2183e4335064541da46cdcaa1d70f4d7
Priyanshi Dhanuka <98470644+priyanshi-158@users.noreply.github.com>
2024-01-30 18:42:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());//middleware
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://pizza-ordersite.vercel.app",
 		credentials:true,
 	})
 )
